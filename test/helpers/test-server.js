40e284a5a0cf0e5cdc4d067cc6ba7b0d75f12d4a
Sam <sam.c.day@gmail.com>
2011-02-17 07:59:50 +1000

    Stuff.

---
+++ test/helpers/test-server.js
@@ -0,0 +1,42 @@
+// A test NodeServ core to test plugins.
+var NodeServ = require("../../lib/"),
+	util = require("util"),
+	path = require("path"),
+	http = require("http");
+
+var port = 10000;
+
+var TestNodeServ = module.exports = function(config) {
+	config = config || {};
+	this.port = config.bind_port = config.bind_port || (port++);
+	config.document_root = config.document_root ||  path.join(__dirname, "..", "fixtures", "www-root");
+	
+	NodeServ.call(this, config);
+	
+	var _self = this;
+	this.on("started", function() {
+		_self.request = function(method, path, callback) {
+			var request = http.request({
+				host: "localhost",
+				port: _self.port,
+				method: method,
+				path: path
+			}, function(res) {
+				res.body = "";
+				res.on("data", function(data) {
+					console.log(data);
+					res.body += data;
+				});
+				res.on("end", function() { 
+					console.log(res);
+				});
+			});
+			//request.end();
+		};
+	});
+};
+util.inherits(TestNodeServ, NodeServ);
+
+TestNodeServ.prototype.toString = function() {
+	return "[Mock NodeServ]";
+};
--- test/test-cgi.js
+++ test/test-cgi.js
@@ -1,27 +1,23 @@
 var vows = require("vows"),
 	assert = require("assert"),
-	MockNodeServer = new(require("./helpers/mock-server")),
+	TestNodeServ = require("./helpers/test-server"),
 	// TODO: change this to import npm gently if felix approves https://github.com/felixge/node-gently/pull/3
 	Gently = require("./helpers/gently"),
-	cgi = require("../lib/plugins/cgi/cgi");
+	cgi = require("../lib/plugins/cgi/cgi"),
+	path = require("path"),
+	events = require("events");
 
 vows.describe("CGI Plugin").addBatch({
-	"When instantiated": {
+	"When called on a mock": {
 		topic: function() {
 			var gently = new Gently(true);
-			var properOn = gently.expect(MockNodeServer, "on", function(event, fn) {
-				//console.log(assert);
-				//properOn.apply(this, arguments);
+			var mock = new events.EventEmitter();
+			mock.config = {};
+			var properOn = gently.expect(mock, "on", function(event, fn) {
 				assert.equal(event, "request_resolve");
-				try {
-					assert.equal(event, "request_resolve");
-				}
-				catch(e) {
-					console.log(e);
-				}
 			}.bind(this));
 
-			cgi(MockNodeServer);
+			cgi(mock);
 			
 			return gently;
 		},
@@ -30,5 +26,23 @@ vows.describe("CGI Plugin").addBatch({
 			gently.verify();
 			//assert.doesNotThrow(gently.verify, Error, "CGI Plugin did not hook request_resolve");
 		}
+	}
+}).addBatch({
+	"When used properly": {
+		topic: function() {
+			var instance = new TestNodeServ({
+				cgi: {
+					".test": path.join(__dirname, "fixtures", "www-root", "cgi", "basic.test")
+				}
+			});
+			
+			instance.start(function() {
+				instance.request("GET", "/cgi/basic.test", function() {
+					
+				});
+			});
+		},
+		
+		"hehe": function() {}
 	}
 }).export(module);
